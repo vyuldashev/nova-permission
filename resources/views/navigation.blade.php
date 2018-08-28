2164ca0bb035e85474220c8325ac798e3e5a22f6
Vladimir Yuldashev <misterio92@gmail.com>
2018-08-28 23:39:35 +0300

    add localization for navigation

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -5,7 +5,7 @@
                   d="M7 10V7a5 5 0 1 1 10 0v3h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h2zm2 0h6V7a3 3 0 0 0-6 0v3zm-4 2v8h14v-8H5zm7 2a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1z"/>
         </svg>
         <span class="sidebar-label">
-            Roles & Permissions
+                @lang('nova-permission-tool::navigation.sidebar-label')
         </span>
     </h3>
 
@@ -19,7 +19,7 @@
                     resourceName: 'roles'
                 }
             }" class="text-white ml-8 no-underline dim">
-                Roles
+                @lang('nova-permission-tool::resources.Roles')
             </router-link>
         </li>
         @endcan
@@ -32,7 +32,7 @@
                     resourceName: 'permissions'
                 }
             }" class="text-white ml-8 no-underline dim">
-                Permissions
+                @lang('nova-permission-tool::resources.Permissions')
             </router-link>
         </li>
         @endcan
