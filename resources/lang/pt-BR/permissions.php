05eee7552489e213498ea584db77e421e85295ff
Lucas Colette <lucas@bubb.com.br>
2018-10-08 20:59:36 -0300

    Support to pt-BR lang

---
+++ resources/lang/pt-BR/permissions.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => 'Nome',
+    'display_name' => 'Mostrar como',
+    'guard_name' => 'Autenticador',
+    'created_at' => 'Criado em',
+    'updated_at' => 'Atualizado em',
+];
---
+++ resources/lang/pt-BR/roles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'name' => 'Nome',
+    'guard_name' => 'Autenticador',
+    'created_at' => 'Criado em',
+    'updated_at' => 'Atualizado em',
+];
