f55347d12d5a4218dda23a72f445f106320f5480
Marco Rivadeneyra <marcorivm@gmail.com>
2018-11-21 18:05:50 -0600

    Add spanish translations
    Signed-off-by: Marco Rivadeneyra <marcorivm@gmail.com>

---
+++ resources/lang/es/permissions.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => 'Nombre',
+    'display_name' => 'Título',
+    'guard_name' => 'Nombre de guardia',
+    'created_at' => 'Fecha de creación',
+    'updated_at' => 'Fecha de actualización',
+];
