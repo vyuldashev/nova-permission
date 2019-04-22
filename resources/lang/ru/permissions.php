78262fcf41a97192caa7bfaa4a640239e7c522bc
Misha.Topol <misha.topol@gmail.com>
2019-04-22 10:50:00 +0300

    fixed_space_in_ru_translation

--- resources/lang/ru/permissions.php
+++ resources/lang/ru/permissions.php
@@ -3,7 +3,7 @@
 return [
     'name' => 'Название',
     'display_name' => 'Отображаемое имя',
-    'guard_name' => 'Имя Guard ',
+    'guard_name' => 'Имя Guard',
     'created_at' => 'Создан',
     'updated_at' => 'Обновлен',
 ];
