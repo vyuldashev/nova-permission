b84244c7d3b871d6657c40c7e0be313bb06dcfc6
astalpaert <arno.stalpaert@hotmail.com>
2019-05-14 21:07:57 +0200

    Translation for the Dutch / NL language

---
+++ resources/lang/nl/permissions.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => 'Naam',
+    'display_name' => 'Weergave naam',
+    'guard_name' => 'Guard Naam',
+    'created_at' => 'Aangemaakt op',
+    'updated_at' => 'Bijgewerkt op',
+];
