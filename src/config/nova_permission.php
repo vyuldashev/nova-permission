19e1062bd6ac7e69d831721bb7bd8b882ad7ba9e
Dennis Mwea <mweadennis2@gmail.com>
2019-08-12 14:16:32 +0300

    Add comments

--- src/config/nova_permission.php
+++ src/config/nova_permission.php
@@ -3,16 +3,17 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Show roles link in the navigation
     |--------------------------------------------------------------------------
     |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
-    |
     */
-
     'show_roles_in_navigation' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Show permissions link in the navigation
+    |--------------------------------------------------------------------------
+    |
+    */
     'show_permissions_in_navigation' => true,
 ];
