863f4b80dcb197b9bd9c716a08d26c346994d9b1
Leandro G. Gehlen <leandrogehlen@gmail.com>
2020-07-02 07:42:20 -0300

    Updated Brazilian translations

--- resources/lang/pt-BR/permissions.php
+++ resources/lang/pt-BR/permissions.php
@@ -6,4 +6,5 @@ return [
     'guard_name' => 'Autenticador',
     'created_at' => 'Criado em',
     'updated_at' => 'Atualizado em',
+    'roles' => 'Grupos'
 ];
--- resources/lang/pt-BR/roles.php
+++ resources/lang/pt-BR/roles.php
@@ -5,4 +5,5 @@ return [
     'guard_name' => 'Autenticador',
     'created_at' => 'Criado em',
     'updated_at' => 'Atualizado em',
+    'permissions' => 'Permissões'
 ];
