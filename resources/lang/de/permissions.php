7001fc8ced8eed7f8777c27159428a7bb939b0c1
Philip Günther <philipalexander@hey.com>
2022-08-19 22:46:34 +0200

    Add missing German translations to permissions lang file

--- resources/lang/de/permissions.php
+++ resources/lang/de/permissions.php
@@ -2,7 +2,9 @@
 
 return [
     'name' => 'Name',
+    'display_name' => 'Anzeigename',
     'guard_name' => 'Guard Name',
     'created_at' => 'Erstellt',
     'updated_at' => 'Aktualisiert',
+    'roles' => 'Rollen',
 ];
